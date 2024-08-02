a54e0f566dd9af20352f34a121205b79604ce301
Qvait <brylov.1996@mail.ru>
2024-08-02 10:45:10 +0500

    Add files via upload

---
+++ Стиль кода часть II. Цикл While.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+a = len(my_list)
+while i < a :
+    for my_list in my_list:
+        i = i + 1 
+        if my_list > -1:
+            print(my_list)
+        else:
+            continue
